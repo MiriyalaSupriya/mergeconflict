50e57d9be4c9e69c05515dd8828b0a122ae09fd6
MiriyalaSupriya <supriya.pancheti@gmail.com>
2022-07-31 18:53:37 +0530

    just wrote a java file and

---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum
+{{{
+    public static void main(String args
+[D[D[D[D[D[D[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[])
+{
+int a,b,res;
+	a=10;
+b=25;[D[D[D[D[D[D[D		
+
+rev=a+b;[D[D[D[D[D[D[D[C[s[C[C[C[C[C[C[C
+
+system.out.println("Addition 10 & 25 is : +[D[D"[C[Cres)
+}
+}
